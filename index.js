32ee43d3e888875c4f0883b7d1150ae16bf15bce
priyanshu-exceleur <priyanshu.agrawal@exceleurservices.com>
2024-08-20 18:01:35 +0530

    sher creating part2

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const mongoose = require('mongoose');
 const server = express()
 const PORT = 8000
 const routes = require("./routes/product")
+require("dotenv").config()
+
 
 server.use(express.json());
 server.use("/", routes)
 
-mongoose.connect('mongodb+srv://priyanshuMongo:Priyanshu%409876@cluster0.yzeye.mongodb.net/myDB');
+mongoose.connect(`${process.env.MONGO_URL}`);
 
 server.listen(PORT, () => {
     console.log("Server Started On 8000 PORT")
